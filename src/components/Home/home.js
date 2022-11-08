f02319c68f4d16ce920a2168e2e168a47b924645
Kyle Patrick Bader <kbader91@gmail.com>
2022-11-08 10:03:41 -0800

    mobile and tablet media queries updated

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -82,10 +82,8 @@ function Home() {
                     </form>
                 </Fade>
                 <Fade right>
-                    <div id="info-and-map">
                         <div id="google-map">
                             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13268.063180590256!2d-117.82638843022458!3d33.7602577!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcdbcd13d3be81%3A0x3d8e1cca1625b0c7!2s13961%20Mauve%20Dr%2C%20Santa%20Ana%2C%20CA%2092705!5e0!3m2!1sen!2sus!4v1667494799764!5m2!1sen!2sus" title="map" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        </div>
                     </div>
                 </Fade>
             </div>
