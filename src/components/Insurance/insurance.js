39340032db1610d727350cef521870708b8af304
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-14 15:37:09 -0800

    media queries consolidated

--- src/components/Insurance/insurance.js
+++ src/components/Insurance/insurance.js
@@ -1,10 +1,12 @@
 import "./insurance.css";
 import Fade from 'react-reveal/Fade';
+import aetna from "../../assets/images/0f0564f1-d965-4e82-9580-862fa8032906.png";
 import anthem from "../../assets/images/Anthem-Inc-logo.png";
 import beacon from "../../assets/images/beacon-logo.png";
 import blueCross from "../../assets/images/bluecross-blueshield-simple-modern-therapy-logo.png";
 import cigna from "../../assets/images/Cigna_logo.svg.png";
-import optima from "../../assets/images/4516741cOptum-logo-ora-RGB.webp"
+import optum from "../../assets/images/4516741cOptum-logo-ora-RGB.webp";
+import optima from '../..//assets/images/optima-health-inc-health-care-health-insurance-png-favpng-dumtEeQ0mCtUM1MyRcuN2YYzZ.webp';
 import horizon from '../../assets/images/horizon-logo.webp';
 import umr from '../../assets/images/umr-logo.webp';
 import unitedH from "../../assets/images/United-Healthcare-Logo.png";
@@ -26,14 +28,16 @@ function Insurance() {
             </div>
             <Fade up>
                 <div id="insurance-logos">
-                    <LazyLoadImage src={anthem} alt="anthem logo" />
-                    <LazyLoadImage src={beacon} alt="anthem logo" />
-                    <LazyLoadImage src={blueCross} alt="anthem logo" />
-                    <LazyLoadImage src={cigna} alt="anthem logo" />
-                    <LazyLoadImage src={optima} alt="logo" />
+                    <LazyLoadImage src={aetna} alt="logo" />
+                    <LazyLoadImage src={anthem} alt="logo" />
+                    <LazyLoadImage src={beacon} alt="logo" />
+                    <LazyLoadImage src={blueCross} alt="logo" />
+                    <LazyLoadImage src={cigna} alt="logo" />
                     <LazyLoadImage src={horizon} alt="logo" />
-                    <LazyLoadImage src={unitedH} alt="logo" />
+                    <LazyLoadImage src={optima} alt="logo" />
+                    <LazyLoadImage src={optum} alt="logo" />
                     <LazyLoadImage src={umr} alt="logo" />
+                    <LazyLoadImage src={unitedH} alt="logo" />
                 </div>
             </Fade>
             <BottomContact />
